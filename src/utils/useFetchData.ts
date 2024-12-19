b1832f5db8be5c36d732f45aa93dace8592e2bb4
Sam Hillier <sam@hillier.uk>
2024-12-19 19:37:01 +0000

    feat: optimise data fetching

--- src/utils/useFetchData.ts
+++ src/utils/useFetchData.ts
@@ -9,5 +9,8 @@ export const useFetchData = () => {
       if (!response.ok) throw new Error("Failed to fetch data");
       return response.json();
     },
+    staleTime: 1000 * 60 * 5,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
   });
 };
